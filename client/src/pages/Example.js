4eb4df4aa13eb629edcbb66eef65bedbae8cb0df
Edward Coad <coaem001@mymail.unisa.edu.au>
2021-09-02 11:57:02 +0930

    Skeleton project, ready for implementation

---
+++ client/src/pages/Example.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Example extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the example component</p>
+      </div>
+    );
+  };
+};
+
+export default Example;
