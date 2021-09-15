8800f80332927b87e058ffc7ed35fd6bfc459a62
Quang Huy Tran <traqy014@mymail.unisa.edu.au>
2021-09-15 18:55:06 +0930

    Add QR Scanning function

--- client/src/components/Terminal.js
+++ client/src/components/Terminal.js
@@ -31,19 +31,19 @@ const Terminal = () => {
     const form = useRef();
     const checkBtn = useRef();
     const [formType, setFormType] = useState("qr");
-    const [username, setUserName] = useState("");
     const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState("");
 
     const [data, setData] = useState({
         username: "",
         name: "",
-        phone: ""
+        phone: "",
+        terminalId: ""
     });
 
     //Handle any changes within the form and set the data object accordingly
     const handleDataChange = (e) => {
-        const {id, value} = e.target;
+        const { id, value } = e.target;
         setData(prevState => ({
             ...prevState,
             [id]: value,
@@ -72,7 +72,7 @@ const Terminal = () => {
             UserService.checkin(data).then(
                 () => {
                     //Put some confirmation of checkin here
-                    window.location.reload();                    
+                    window.location.reload();
                 },
                 (error) => {
                     const resMessage =
@@ -92,89 +92,136 @@ const Terminal = () => {
 
     }
     const handleDecode = (result) => {
-        console.log(result);
+        console.log(result.data);
+        if (result && result !== "") {
+            setFormType("confirm");
+            setData(prevState => ({
+                ...prevState,
+                username: result.data,
+            }));
+            // handleSumitScan();          
+        }
     }
 
     const handleScannerLoad = (mode) => {
         console.log(mode);
     }
 
+    const handleSumitScan = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        data.terminalId = urlParams.get('id');
+        UserService.checkin(data).then(
+            () => {
+                //Put some confirmation of checkin here
+                window.location.reload();
+            },
+            (error) => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+
+                setLoading(false);
+                setMessage(resMessage);
+            }
+        );
+    };
     return (
         <div className="container">
             <header className="jumbotron">
                 <h3>{content}</h3>
             </header>
-            <div className="card card-container">
 
-                <div className="card-title">
-                    COVID-19 CHECKIN
-                </div>
-
-                {formType === "qr" ? (
-                    <div>
-                        <button className="btn btn-primary btn-block" onClick={() => handleFormType("manual")} >
-                            Switch To Manual Check-in
-                        </button>
-                        <Scanner
-                            className="camera"
-                            onDecode={handleDecode}
-                            onScannerLoad={handleScannerLoad}
-                            constraints={{
-                                audio: false,
-                                video: {
-                                    facingMode: "environment"
-                                }
-                            }}
-                            captureSize={{ width: 1280, height: 720 }}
-                        />
-                    </div>
-                ) :
-                    (
-                        <div>
-                            <button className="btn btn-primary btn-block" onClick={() => handleFormType("qr")}>
-                                Switch To QR Scanning
-                            </button>
-                            <Form onSubmit={handleSubmit} ref={form}>
-                                <div className="form-group" >
-                                    <label htmlFor="name">Full Name</label>
-                                    <Input
-                                        type="text"
-                                        className="form-control"
-                                        name="name"
-                                    />
-                                </div>
-                                <div className="form-group" >
-                                    <label htmlFor="phone">Phone Number</label>
-                                    <Input
-                                        type="text"
-                                        className="form-control"
-                                        name="phone"
-                                    />
-                                </div>
-
-
-                                <div className="form-group" style={{ marginTop: 20 }}>
-                                    <button className="btn btn-primary btn-block" disabled={loading || !validateForm()}>
-                                        {loading && (
-                                            <span className="spinner-border spinner-border-sm"></span>
-                                        )}
-                                        <span>Check In</span>
-                                    </button>
-                                </div>
-
-                                {message && (
-                                    <div className="form-group">
-                                        <div className="alert alert-danger" role="alert">
-                                            {message}
-                                        </div>
-                                    </div>
-                                )}
-
-                                <CheckButton style={{ display: "none" }} ref={checkBtn} />
-                            </Form>
-                        </div>
-                    )}
+            <div className="card-title">
+                COVID-19 CHECKIN
             </div>
+
+            {formType === "qr" && (
+                <div>
+                    <label>Scanning QR Code</label>
+                    
+                    <Scanner
+                        className="camera border border-dark"
+                        onDecode={handleDecode}
+                        onScannerLoad={handleScannerLoad}
+                        constraints={{
+                            audio: false,
+                            video: {
+                                facingMode: "environment"
+                            }
+                        }}
+                        captureSize={{ width: 1280, height: 720 }}
+                    />
+                    <button className="btn btn-primary btn-block" onClick={() => handleFormType("manual")}>
+                        <span>Switch To Manual Check-in</span>
+                    </button>
+                </div>
+            )}
+            {formType === "manual" && (
+                <div>
+
+                    <Form onSubmit={handleSubmit} ref={form}>
+                        <div className="form-group" >
+                            <label htmlFor="name">Full Name</label>
+                            <Input
+                                type="text"
+                                className="form-control"
+                                name="name"
+                                id="name"
+                                value={data.name}
+                                onChange={handleDataChange}
+                            />
+                        </div>
+                        <div className="form-group" >
+                            <label htmlFor="phone">Phone Number</label>
+                            <Input
+                                type="text"
+                                className="form-control"
+                                name="phone"
+                                id="phone"
+                                value={data.phone}
+                                onChange={handleDataChange}
+                            />
+                        </div>
+
+
+                        <div className="form-group" style={{ marginTop: 20 }}>
+                            <button className="btn btn-primary btn-block" disabled={loading || !validateForm()}>
+                                {loading && (
+                                    <span className="spinner-border spinner-border-sm"></span>
+                                )}
+                                <span>Check In</span>
+                            </button>
+                        </div>
+
+                        {message && (
+                            <div className="form-group">
+                                <div className="alert alert-danger" role="alert">
+                                    {message}
+                                </div>
+                            </div>
+                        )}
+
+                        <CheckButton style={{ display: "none" }} ref={checkBtn} />
+                    </Form>
+
+                    <button className="btn btn-primary btn-block" onClick={() => handleFormType("qr")} style={{ marginTop: 20}} >
+                        <span>Switch To QR Scanning</span>
+                    </button>
+                </div>
+            )}
+            {
+                formType === "confirm" && (
+                    <div>
+
+                        <label>{"Username: " + data.username}</label>
+                        <button className="btn btn-primary" onClick={handleSumitScan} >Confirm</button>
+                        <button className="btn btn-primary" onClick={() => setFormType("qr")} style={{ marginLeft: 10 }} >Go back To Check In</button>
+                    </div>
+                )
+            }
         </div>
     );
 };
