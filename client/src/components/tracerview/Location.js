0adf8d2db369deb0a6e477cdc0cef2b3c29df994
Quang Huy Tran <traqy014@mymail.unisa.edu.au>
2021-09-30 17:26:45 +0930

    Change the style of display list of checkin for tracer

--- client/src/components/tracerview/Location.js
+++ client/src/components/tracerview/Location.js
@@ -9,7 +9,7 @@ const Location = () => {
     const [accounts, setAccounts] = useState([]);
 
     const handleDataChange = (e) => {
-        const {id, value} = e.target;
+        const { id, value } = e.target;
         setData(prevState => ({
             ...prevState,
             [id]: value,
@@ -21,7 +21,7 @@ const Location = () => {
 
         console.log(data);
 
-        if(data.lcoation == "") {
+        if (data.lcoation == "") {
             return;
         }
 
@@ -42,35 +42,49 @@ const Location = () => {
 
             <div className="form-group mb-2">
                 <label>Specific Location</label>
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    placeholder="Please enter location name" 
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Please enter location name"
                     id="location"
                     value={data.location}
-                    onChange={handleDataChange}/>
+                    onChange={handleDataChange} />
             </div>
 
             <div class="text-center">
-            <button onClick={handleSearch} type="button" class="btn btn-primary btn-lg btn-block mb-5">CHECK</button>
-            </div>   
+                <button onClick={handleSearch} type="button" class="btn btn-primary btn-lg btn-block mb-5">CHECK</button>
+            </div>
 
-            {accounts.length > 0 ? (accounts.map(account => {
-                console.log(account);
-                let id = account.userId; //only for registered users
-                let name = account.name;
-                let phone = account.phone;
-                let createdAt = account.createdAt;
-                
-                return (
-                    <li id={id}>
-                        <strong>Name:</strong> {name} <br></br>
-                        <strong>Phone:</strong> {phone} <br></br>
-                        <strong>Time:</strong> {createdAt}
-                    </li>
-                )
-            }) ) : (
-                <li></li>
+            {accounts.length > 0 ? (
+                <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {accounts.map(account => {
+                                let id = account.userId; //only for registered users
+                                let name = account.name;
+                                let phone = account.phone;
+                                let createdAt = account.createdAt;
+
+                                return (
+                                    <tr id={id}>
+                                        <td>{name}</td>
+                                        <td>{phone}</td>
+                                        <td>{createdAt.toLocaleString()}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            ) : (
+                <p></p>
             )}
 
         </form>
--- client/src/components/tracerview/SpecificPerson.js
+++ client/src/components/tracerview/SpecificPerson.js
@@ -11,7 +11,7 @@ const SpecificPerson = () => {
     const [accounts, setAccounts] = useState([]);
 
     const handleDataChange = (e) => {
-        const {id, value} = e.target;
+        const { id, value } = e.target;
         setData(prevState => ({
             ...prevState,
             [id]: value,
@@ -23,7 +23,7 @@ const SpecificPerson = () => {
 
         console.log(data);
 
-        if(data.phone == "" && data.username == "") {
+        if (data.phone == "" && data.username == "") {
             return;
         }
 
@@ -50,53 +50,67 @@ const SpecificPerson = () => {
 
             <div className="form-group">
                 <label>Phone</label>
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    placeholder="Please enter the phone number" 
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Please enter the phone number"
                     id="phone"
                     value={data.phone}
-                    onChange={handleDataChange}/>
+                    onChange={handleDataChange} />
             </div>
-            
+
             <div class="text-center my-3">
-            <p > 
-                <b> OR </b>
-            </p>
+                <p >
+                    <b> OR </b>
+                </p>
             </div>
 
             <div className="form-group mb-2">
                 <label>Unique Identifier</label>
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    placeholder="Please enter the unique identifier" 
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Please enter the unique identifier"
                     id="username"
                     value={data.username}
-                    onChange={handleDataChange}/>
+                    onChange={handleDataChange} />
             </div>
 
 
             <div class="text-center">
                 <button onClick={handleSearch} type="button" class="btn btn-primary btn-lg btn-block mb-5">CHECK</button>
-            </div>    
+            </div>
 
-            {accounts.length > 0 ? (accounts.map(account => {
-                console.log(account);
-                let id = account.userId; //only for registered users
-                let location = account.location;
-                let createdAt = account.createdAt;
-                
-                return (
-                    <li id={id}>
-                        <strong>Location:</strong> {location} <br></br>
-                        <strong>Time:</strong> {createdAt}
-                    </li>
-                )
-            })) : (
-                <li></li>
+            {accounts.length > 0 ? (
+                <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                {/* <th scope="col">Name</th> */}
+                                <th scope="col">Location</th>
+                                <th scope="col">Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {accounts.map(account => {
+                                let id = account.userId; //only for registered users
+                                let location = account.location;
+                                let createdAt = account.createdAt;
+
+                                return (
+                                    <tr id={id}>
+                                        <td>{location}</td>
+                                        <td>{createdAt.toLocaleString()}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            ) : (
+                <p></p>
             )}
-                    
+
         </form>
     );
 }
